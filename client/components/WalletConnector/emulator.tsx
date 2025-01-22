75cdbd2b9d475b193cf4f3a416f42e1dacbc8fd2
Cryptonean <faizanvit8@gmail.com>
2025-01-22 11:44:14 +0530

    wallet connection

--- client/components/WalletConnector/emulator.tsx
+++ client/components/WalletConnector/emulator.tsx
@@ -1,96 +1,97 @@
-'use client'
-import { useEffect, useState } from 'react'
-import { Spinner } from '@heroui/spinner'
-import { Snippet } from '@heroui/snippet'
+"use client";
+import { useEffect, useState } from "react";
+import { Spinner } from "@heroui/spinner";
+import { Snippet } from "@heroui/snippet";
 import {
   EmulatorAccount,
   Lucid,
   paymentCredentialOf,
-} from '@lucid-evolution/lucid'
-import { Skeleton } from '@heroui/skeleton'
+} from "@lucid-evolution/lucid";
+// import { Skeleton } from '@heroui/skeleton'
 
-import { Button } from '../ui/button'
+import { Button } from "../ui/button";
 
-import { handleError } from '@/lib/utils'
-import { useWallet } from '@/context/walletContext'
+import { handleError } from "@/lib/utils";
+import { useWallet } from "@/context/walletContext";
 import {
   accountA,
   accountB,
   accountC,
   accountD,
   emulator,
-} from '@/config/emulator'
+} from "@/config/emulator";
+import { Skeleton } from "@heroui/skeleton";
 
 export default function EmulatorConnectors() {
-  const [walletConnection, setWalletConnection] = useWallet()
-  const { lucid } = walletConnection
+  const [walletConnection, setWalletConnection] = useWallet();
+  const { lucid } = walletConnection;
 
-  const [wallets, setWallets] = useState<EmulatorAccount[]>()
+  const [wallets, setWallets] = useState<EmulatorAccount[]>();
 
-  let isInit = false
+  let isInit = false;
 
   useEffect(() => {
-    if (isInit) return
-    else isInit = true
-    Lucid(emulator, 'Custom')
+    if (isInit) return;
+    else isInit = true;
+    Lucid(emulator, "Custom")
       .then((lucid) => {
         setWalletConnection((walletConnection) => {
-          return { ...walletConnection, lucid }
-        })
-        setWallets([accountA, accountB, accountC, accountD])
+          return { ...walletConnection, lucid };
+        });
+        setWallets([accountA, accountB, accountC, accountD]);
       })
       .catch((error) =>
         // toast error
         console.log(error)
-      )
-  }, [])
+      );
+  }, []);
 
   async function onConnectWallet(account: EmulatorAccount) {
     try {
-      if (!lucid) throw 'Uninitialized Lucid!!!'
-      lucid.selectWallet.fromSeed(account.seedPhrase)
-      const address = await lucid.wallet().address()
+      if (!lucid) throw "Uninitialized Lucid!!!";
+      lucid.selectWallet.fromSeed(account.seedPhrase);
+      const address = await lucid.wallet().address();
 
       setWalletConnection((walletConnection) => {
-        return { ...walletConnection, address }
-      })
-      console.log('connected emulator wallet')
+        return { ...walletConnection, address };
+      });
+      console.log("connected emulator wallet");
     } catch (error) {
-      handleError(error)
+      handleError(error);
     }
   }
 
   function log() {
-    emulator.log()
+    emulator.log();
   }
 
   function awaitEmulator() {
-    emulator.awaitBlock(1)
-    console.log('Current block height', emulator.blockHeight)
+    emulator.awaitBlock(1);
+    console.log("Current block height", emulator.blockHeight);
   }
   if (!wallets)
     return (
-      <Snippet hideCopyButton hideSymbol variant='bordered'>
-        <Spinner label='Browsing Cardano Wallets' />
+      <Snippet hideCopyButton hideSymbol variant="bordered">
+        <Spinner label="Browsing Cardano Wallets" />
       </Snippet>
-    )
+    );
 
   if (!wallets.length)
     return (
-      <Snippet hideCopyButton hideSymbol variant='bordered'>
-        <p className='uppercase'>No Cardano Wallet</p>
+      <Snippet hideCopyButton hideSymbol variant="bordered">
+        <p className="uppercase">No Cardano Wallet</p>
       </Snippet>
-    )
+    );
 
   return (
-    <div className='flex flex-col gap-4 w-full items-center'>
+    <div className="flex flex-col gap-4 w-full items-center">
       {wallets.map((wallet, w) => {
         return (
           <>
-            <Skeleton key={w} className='rounded-full' isLoaded={!!lucid}>
+            <Skeleton key={w} className="rounded-full" isLoaded={!!lucid}>
               <Button
-                className='capitalize'
-                color='primary'
+                className="capitalize"
+                color="primary"
                 onClick={() => onConnectWallet(wallet)}
               >
                 {`${wallet.address.slice(0, 30)}...${wallet.address.slice(-5)}`}
@@ -98,10 +99,12 @@ export default function EmulatorConnectors() {
               {paymentCredentialOf(wallet.address).hash}
             </Skeleton>
           </>
-        )
+        );
       })}
-      <Button onClick={log}>Log</Button>
-      <Button onClick={awaitEmulator}>Await</Button>
+      <div className="flex gap-4">
+        <Button onClick={log}>Log</Button>
+        <Button onClick={awaitEmulator}>Await</Button>
+      </div>
     </div>
-  )
+  );
 }
